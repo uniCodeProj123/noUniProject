a404bb155b17f2a4cabfba337ac60772c0613cd4
Hussain <anas.hussain@capgemini.com>
2020-07-30 22:49:04 +0100

    model author

--- models/author.js
+++ models/author.js
@@ -1,44 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const Author = require('../models/author')
+const mongoose = require('mongoose')
 
-// All Authors Route
-router.get('/', async (req, res) => {
-  let searchOptions = {}
-  if (req.query.name != null && req.query.name !== '') {
-    searchOptions.name = new RegExp(req.query.name, 'i')
-  }
-  try {
-    const authors = await Author.find(searchOptions)
-    res.render('authors/index', {
-      authors: authors,
-      searchOptions: req.query
-    })
-  } catch {
-    res.redirect('/')
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   }
 })
 
-// New Author Route
-router.get('/new', (req, res) => {
-  res.render('authors/new', { author: new Author() })
-})
-
-// Create Author Route
-router.post('/', async (req, res) => {
-  const author = new Author({
-    name: req.body.name
-  })
-  try {
-    const newAuthor = await author.save()
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect(`authors`)
-  } catch {
-    res.render('authors/new', {
-      author: author,
-      errorMessage: 'Error creating Author'
-    })
-  }
-})
-
-module.exports = router
+module.exports = mongoose.model('Author', authorSchema)
