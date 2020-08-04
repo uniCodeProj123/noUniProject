19b1b1461d8b3435e5cdcaf9b82c4ce453ae02ba
Hussain <anas.hussain@capgemini.com>
2020-08-04 11:56:51 +0100

    remove gif format

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Book = require('../models/book')
 const Author = require('../models/author')
 
-const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
+const imageMimeTypes = ['image/jpeg', 'image/png']
 
 // All Books Route
 router.get('/', async (req, res) => {
